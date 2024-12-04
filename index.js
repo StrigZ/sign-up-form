9af67367124912a27e5772900869687d9872d2b0
StrigZ <mirgasan200@gmail.com>
2024-12-04 15:50:09 +0300

    Add form validation

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const form = document.querySelector("form");
+const passwordInput = document.querySelector("#password");
+const passwordConfirmInput = document.querySelector("#confirm-password");
+const passwordMatchParagraph = document.querySelector("#password-match");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(e);
+});
+
+passwordConfirmInput.addEventListener("change", (e) => {
+  if (passwordInput.value === e.target.value) {
+    passwordMatchParagraph.classList.add("hidden");
+  } else {
+    passwordMatchParagraph.classList.remove("hidden");
+  }
 });
